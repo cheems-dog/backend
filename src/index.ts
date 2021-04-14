c503183d05559e8f0fd91cac275f3f6669698dc3
JuzioMiecio520 <50195535+JuzioMiecio520@users.noreply.github.com>
2021-04-14 17:57:50 +0200

    :sparkles: Initial code
    Added initial code to start a project

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import * as express from 'express';
+import * as chalk from 'chalk';
+import * as logSymbols from 'log-symbols';
+import * as path from 'path';
+import * as fileUpload from 'express-fileupload';
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(fileUpload({
+    safeFileNames: true,
+    preserveExtension: true
+}));
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.render('index');
+});
+
+app.listen(84, () => {
+    console.log(`${chalk.white.bgGreen(logSymbols.success)} Server started on port ${chalk.cyan('80')}`);
+});
