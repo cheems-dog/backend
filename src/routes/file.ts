893b887638aa1f68bef714535af4dbf5ea8d1a3a
JuzioMiecio520 <50195535+JuzioMiecio520@users.noreply.github.com>
2021-04-29 08:20:13 +0200

    "GET /:file" rendering fix

--- src/routes/file.ts
+++ src/routes/file.ts
@@ -1,22 +1,27 @@
 import * as express from 'express';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as mongoose from 'mongoose';
 import * as functions from '../functions';
+import { UserModel } from '../models/user';
+import { ImageModel } from '../models/image';
 
 const config = functions.config();
 
 const router = express.Router();
 
-router.get('/:file([a-zA-Z0-9\-\_]+(\.)(jpeg|jpg|png|gif|webm|mp4))', (req: express.Request, res: express.Response) => {
+router.get('/:file([a-zA-Z0-9\-\_]+(\.)(jpeg|jpg|png|gif|webm|mp4))', async (req: express.Request, res: express.Response) => {
     const filename = req.params['file'] + req.params['1'];
 
-    if(!fs.existsSync(path.join(__dirname, '../storage', filename))) return res.status(404);
+    if (!fs.existsSync(path.join(__dirname, '../storage', filename))) return res.status(404);
+
+    const file = await ImageModel.findOne({ name: filename }).exec();
+    const author = await UserModel.findById(file.author).exec();
 
     //res.send(`<h1>${filename}</h1><img src="/static/storage/${filename}">`);
     res.render('file', {
-        file: {
-            filename
-        }
+        file,
+        author
     });
 });
 
